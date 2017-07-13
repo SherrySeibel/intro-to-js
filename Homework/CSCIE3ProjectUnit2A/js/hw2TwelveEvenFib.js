9b8fc5622a4cbf2e1e41d253b35240c2ce3db2c9
Sherry Seibel <sherry.seibel@gmail.com>
2017-07-12 22:11:03 -0400

    Sum the first 12 evens of fib

--- Homework/CSCIE3ProjectUnit2A/js/hw2TwelveEvenFib.js
+++ Homework/CSCIE3ProjectUnit2A/js/hw2TwelveEvenFib.js
@@ -17,9 +17,10 @@
 var getFibSum = document.getElementById("sumFib");
 
 //then we set the event handler for when the button is clicked
-getFibSum.onclick = function(){
-               document.getElementById("sumFibResult").innerHTML = twelveEvenFibonacciSum();
- }
+getFibSum.onclick = function() {
+  document.getElementById("sumFibResult").innerHTML = twelveEvenFibonacciSum();
+  console.log('hi')
+}
 
  /*
   *  twelveEvenFibonacciSum - calulates the sum of the first 12 even fibonacci numbers, with 0, 1 being the first two numbers of the sequence
@@ -27,7 +28,20 @@ getFibSum.onclick = function(){
   *            @returns {integer} The sum of the first 12 even Fibonacci numbers
   */
 
- function twelveEvenFibonacciSum(){
- /// WRITE YOUR CODE HERE
-    
- } 
+ function twelveEvenFibonacciSum() {
+   var total = 0;
+   var fib = [];
+   fib[0] = 0;
+   fib[1] = 1;
+
+   for (var i = 2; i <= 33; i++) {
+     fib[i] = fib[i - 1] + fib[i -2];
+
+     if (fib[i] % 2 === 0) {
+       total += fib[i];
+       console.log(total);
+      }
+   }
+
+   return total;
+ }
